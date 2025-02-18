2ccf3659ceb961f2ec003fc910d08076db91cd4b
mona952002 <monasalha22@gmail.com>
2025-02-18 19:23:44 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    $name = 'mona';
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    // return view('about')-> with('name',$name);
+   // return view('about', ['name' => $name]);
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
+
