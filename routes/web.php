498f6d1b490a1539ad761227d5114b6330a4d453
mona952002 <monasalha22@gmail.com>
2025-02-23 16:36:07 +0200

    third commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -41,11 +42,29 @@ Route::post('/about', function () {
 });
 
 Route::get('tasks', function () {
-    return view('tasks');
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('tasks'));
 });
 
 Route::post('create', function () {
     $task_name = $_POST['name'];
     DB::table('tasks')->insert(['name' => $task_name]);
-    return view('tasks');
+    return redirect()->back();
+});
+
+Route::post('delete/{id}', function ($id) {
+    DB::table('tasks')->where('id', $id)->delete();
+    return redirect()->back();
+});
+
+Route::post('edit/{id}', function ($id) {
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('task', 'tasks'));
+});
+
+Route::post('update', function () {
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id','=', $id)->update(['name' => $_POST['name']]);
+    return redirect('tasks');
 });
